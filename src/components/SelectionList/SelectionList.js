14ddea9d241118b17688775fc20f966a33c0bc93
SAURABH SONI <129734631+dev-saurabh-soni@users.noreply.github.com>
2024-01-16 10:59:18 +0530

    learnt useEffect

--- src/components/SelectionList/SelectionList.js
+++ src/components/SelectionList/SelectionList.js
@@ -3,38 +3,44 @@ import './SelectionList.css';
 
 
 const SelectionList = () => {
-    const [ list, setList] = useState([])
+    const [list, setList] = useState([])
+    const [contacts, setContacts] = useState([])
 
-    const contacts = [
-        { id: 0, name: "Datta", phone: 56694738 },
-        { id: 1, name: "Nisha", phone: 83740565 },
-        { id: 2, name: "sanket", phone: 8576647774 },
-        { id: 3, name: "Abhishak", phone: 8576647774 },
-        { id: 4, name: "Meghna", phone: 8576647774 },
-        { id: 5, name: "Rutvik", phone: 8576647774 },
-        { id: 6, name: "Kirti", phone: 8576647774 },
 
-    ]
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((json) => {
+                setContacts(json)
+            });
+    }, []);
 
+
+    const addElement = (name) => {
+        const index = list.findIndex((item) => item === name)
+        if (index === -1) setList([...list, name])
+        else {
+            list.splice(index, 1)
+            setList(list)
+        }
+    }
 
     return (
-       <div>
-        {
-            contacts.map((item)=>{
-                return(
-                    <div key={item?.id}  className='maincontainer'>
-                      <div>
-                         <div className='name'>{item?.name}</div>
-                       <div className='phone'>{item?.phone}</div>
-                      </div>
-                      <input type='checkbox' onClick={()=>{
-                       setList([...list, item?.name])
-                      }}/>
-                    </div>
-                )
-            })
-        }
-       </div>
+        <div>
+            {
+                contacts?.map((item) => {
+                    return (
+                        <div key={item?.id} className='maincontainer'>
+                            <div>
+                                <div className='name'>{item?.name}</div>
+                                <div className='phone'>{item?.phone}</div>
+                            </div>
+                            <input type='checkbox' onClick={() => { addElement(item?.name) }} />
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
 
